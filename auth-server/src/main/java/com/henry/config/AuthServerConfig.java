5bc993eeee35ec03a321e66d2d9959a0d7c63f20
Henry <henry.pham@alpaca.vn>
2024-11-01 09:24:00 +0700

    build: init project v1.4

--- auth-server/src/main/java/com/henry/config/AuthServerConfig.java
+++ auth-server/src/main/java/com/henry/config/AuthServerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +13,8 @@ public class AuthServerConfig extends BaseObjectLoggAble {
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/authenticate/**").permitAll()
-                        .anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
-
-        return http.build();
+//       http
+        return null;
     }
+
 }
