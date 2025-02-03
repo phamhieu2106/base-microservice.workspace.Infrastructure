5198c4963c30578c197fc4f95e4dd3f52e995f4f
Henry <henry.pham@alpaca.vn>
2025-02-04 00:40:09 +0700

    build: init project v3

--- auth-server/src/main/java/com/henry/AuthServerApplication.java
+++ auth-server/src/main/java/com/henry/AuthServerApplication.java
@@ -1,13 +1,20 @@
 package com.henry;
 
+import com.henry.config.GradlePropertiesLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication(scanBasePackages = "com.henry")
 @EnableFeignClients
 public class AuthServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AuthServerApplication.class, args);
+        SpringApplication application = new SpringApplication(AuthServerApplication.class);
+        application.addInitializers(applicationContext -> {
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            new GradlePropertiesLoader(environment).loadGradleProperties();
+        });
+        application.run(args);
     }
 }
--- notification-server/src/main/java/com/henry/NotificationServerApplication.java
+++ notification-server/src/main/java/com/henry/NotificationServerApplication.java
@@ -1,11 +1,18 @@
 package com.henry;
 
+import com.henry.config.GradlePropertiesLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class NotificationServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotificationServerApplication.class, args);
+        SpringApplication application = new SpringApplication(NotificationServerApplication.class);
+        application.addInitializers(applicationContext -> {
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            new GradlePropertiesLoader(environment).loadGradleProperties();
+        });
+        application.run(args);
     }
 }
