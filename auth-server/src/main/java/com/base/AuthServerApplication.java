fe1f5d6f7ec5b8a54a2c148dafed7d5e70240377
Henry <henry.pham@alpaca.vn>
2025-06-30 18:05:58 +0700

    build: update base config

--- auth-server/src/main/java/com/base/AuthServerApplication.java
+++ auth-server/src/main/java/com/base/AuthServerApplication.java
@@ -3,12 +3,29 @@ package com.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.base")
 @EnableFeignClients
+@EnableSwagger2
 public class AuthServerApplication {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(AuthServerApplication.class);
-        application.run(args);
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.base.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Auth Server API").description("Auth Server API v1.0").build());
     }
 }
