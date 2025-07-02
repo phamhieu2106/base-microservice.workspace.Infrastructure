779a88e856e580a9a850c463734bb0e53c646e9b
Henry <henry.pham@alpaca.vn>
2025-07-02 14:34:46 +0700

    build: rebuild project

--- auth-server/src/main/java/com/base/AuthServerApplication.java
+++ auth-server/src/main/java/com/base/AuthServerApplication.java
@@ -2,28 +2,10 @@ package com.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.base")
-@EnableSwagger2
 public class AuthServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.base.resource"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(new ApiInfoBuilder().version("1.0").title("Auth Server API").description("Auth Server API v1.0").build());
     }
 }
