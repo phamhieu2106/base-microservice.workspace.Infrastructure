3269b1f4b8437d3f32be7ded2461db84b2131cbf
Henry <henry.pham@alpaca.vn>
2025-07-02 15:00:09 +0700

    build: rebuild project

--- auth-server/src/main/java/com/base/function/SignUpFunc.java
+++ auth-server/src/main/java/com/base/function/SignUpFunc.java
@@ -3,6 +3,7 @@ package com.base.function;
 import com.base.CacheUtils;
 import com.base.common.CommonConstant;
 import com.base.constant.AuthErrorCode;
+import com.base.exception.ServiceException;
 import com.base.func.BaseFunc;
 import com.base.request.SignUpRequest;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +17,13 @@ public class SignUpFunc extends BaseFunc {
 
     public String exec(SignUpRequest request) {
         String username = request.getUsername();
-
-        if (cacheUtils.exists(CommonConstant.AuthKey.USER + username)) {
-            throw new SecurityException(AuthErrorCode.USERNAME_EXIT);
+        if (cacheUtils.isSetMember(CommonConstant.AuthKey.ACTIVE_USERNAME, username)) {
+            logger.error(">>>>>>> Username is already in use with username={} ", username);
+            throw new ServiceException(AuthErrorCode.USERNAME_EXIT);
         }
 
 
-        cacheUtils.store(CommonConstant.AuthKey.USER + username, username);
+        cacheUtils.addToSet(CommonConstant.AuthKey.ACTIVE_USERNAME, username);
         return username;
     }
 }
